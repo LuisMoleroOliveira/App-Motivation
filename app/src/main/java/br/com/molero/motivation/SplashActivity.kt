643414aa90022b64e5d6a378e977f5684af1a9a8
Luis Molero <luis.molero@outlook.com>
2021-10-13 14:59:20 -0300

    Splash Activity

---
+++ app/src/main/java/br/com/molero/motivation/SplashActivity.kt
@@ -0,0 +1,15 @@
+package br.com.molero.motivation
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if(supportActionBar != null) supportActionBar!!.hide()
+
+    }
+}
